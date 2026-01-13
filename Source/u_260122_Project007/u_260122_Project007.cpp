7edb845eb1adf38b0428e45272b34a048b6874b0
Bill Lee <ood11611doo@gmail.com>
2026-01-13 10:25:15 +0900

    260113 - Initialization Initial upload

---
+++ Source/u_260122_Project007/u_260122_Project007.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "u_260122_Project007.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, u_260122_Project007, "u_260122_Project007" );
